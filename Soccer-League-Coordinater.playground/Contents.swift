efef89b15f06a3dcd8c481a64f03996812f68f05
Jaleel Blue <jaleel.blue97@gmail.com>
2019-05-15 14:45:47 -0400

    Fixed bug that caused name of the players team to be their actual name
    Added more functionality to the application such as an average height report, composing letters, and returning letters

--- Soccer-League-Coordinater.playground/Contents.swift
+++ Soccer-League-Coordinater.playground/Contents.swift
@@ -71,6 +71,15 @@ func getAverageHeight(from team: [[String: String]]) -> Double {
 }
 
 /*
+ takes in a string and returns a report of the average height of that team
+ */
+func reportAverageHeightFor(team: [[String : String]], name: String) -> String{
+    let averageHeight = team[team.count - 1]["Average Height"]!
+    
+    return "Team \(name) = \(averageHeight)"
+}
+
+/*
  Takes in an Array of players each represented by a dictionary and returns
  a tuple containing three arrays each representing a team made from the players, such
  that each team contains the same number of experienced players
@@ -81,9 +90,12 @@ func makeTeam(from players: [[String : String]]) -> ([[String : String]], [[Stri
     var dragons: [[String: String]] = []
     var raptors: [[String: String]] = []
     
-    var teamSwitch : Int = -1
+    var teamSwitch : Int = -1 /*-1, 0, and 1 represent sharks, dragons and raptors
+                                respectively
+                              */
     
     for player in players {
+        // distribute the experienced players
         let experienced = player["experienced"]!
         if  experienced == "true"{
             switch teamSwitch {
@@ -93,6 +105,9 @@ func makeTeam(from players: [[String : String]]) -> ([[String : String]], [[Stri
             default: teamSwitch = -1
  
             } } else {
+            // the list of players is sorted based on height prior to
+            // being input into this function so we do not check for heigth when
+            // distributing the players
             switch teamSwitch {
             case -1: sharks.append(player); teamSwitch = 0
             case 0: dragons.append(player); teamSwitch = 1
@@ -108,6 +123,104 @@ func makeTeam(from players: [[String : String]]) -> ([[String : String]], [[Stri
  
                                                         
     return (sharks, dragons, raptors)
+}
+
+
+
+
+/*
+ Takes a team name and a team, and iterates through the team to produce letters
+ to the guardians of each player
+ */
+
+func composeLettersFrom(team: [[String : String]], for name: String) -> [String]{
+    var letters: [String] = []
+    switch name {
+    case "Sharks":
+        // Uses the half open range operator so that index will stop shy of
+        // the last index for team since that index contains the dictionary that
+        // holds the Average Height of the team and will result in an error
+        for index in 0..<team.count - 1 {
+            let sharkPlayer = team[index]
+            let guardian = sharkPlayer["guardians"]!
+            let playerName = sharkPlayer["name"]!
+            let letter = "Dear \(guardian) your child \(playerName)"
+                        + " is on team \(name) so their team meets for practice on " +
+                        "March 17, 3pm"
+            letters.append(letter)
+        }
+    case "Dragons":
+        for index in 0..<team.count - 1 {
+            let dragonPlayer = team[index]
+            let guardian = dragonPlayer["guardians"]!
+            let playerName = dragonPlayer["name"]!
+            let letter = "Dear \(guardian) your child \(playerName)"
+                + " is on team \(name) so their team meets for practice on " +
+            "March 17, 1pm"
+            letters.append(letter)
+        }
+    case "Raptors":
+        for index in 0..<team.count - 1 {
+            let raptorPlayer = team[index]
+            let guardian = raptorPlayer["guardians"]!
+            let playerName = raptorPlayer["name"]!
+            let letter = "Dear \(guardian) your child \(playerName)"
+                + " is on team \(name) so their team meets for practice on " +
+            "March 18, 1pm"
+            letters.append(letter)
+        }
+    default: return letters
+        
+    }
+    // Strings are added to the front and back of the letters array so
+    // that printing is easier later on
+    letters.insert("-----------------\(name)-------------------", at: 0)
+    letters.append("-------------------------------------------")
+    return letters
+}
+
+
+/*
+ Takes a tuple representing teams and returns a tuple
+ containing arrays of letters to the guardians of each player
+ based on the team the player is on
+ */
+func makeLetters(from teams: ([[String : String]], [[String : String]],[[String : String]]))
+    -> ([String], [String], [String])
+{
+    
+    let sharks = teams.0
+    let dragons = teams.1
+    let raptors = teams.2
+    
+    let sharkLetters : [String] = composeLettersFrom(team: sharks, for: "Sharks")
+    let dragonLetters : [String] = composeLettersFrom(team: dragons, for: "Dragons")
+    let raptorLetters : [String] = composeLettersFrom(team: raptors, for: "Raptors")
+    
+    return (sharkLetters, dragonLetters, raptorLetters)
+    
+}
+
+
+/*
+ Takes in a tuple containing letters and outputs them
+ */
+func outputLetters(letters : ([String], [String], [String])){
+    let sharkLetters = letters.0
+    let dragonLetters = letters.1
+    let raptorLetters = letters.2
+    
+    for letter in sharkLetters{
+        print(letter)
+    }
+    
+    for letter in dragonLetters{
+        print(letter)
+    }
+    
+    for letter in raptorLetters{
+        print(letter)
+    }
 }
 
 /*
@@ -134,9 +247,21 @@ let players: [[String: String]] = [["name" : "Joe Smith", "height" : "42.0",
             ["name": "Herschel Krustofski", "height": "45.0", "experienced": "true", "guardians": "Hyman and Rachel Krustofski" ]]
 
 
-var teams = makeTeam(from: sortPlayersByHeight(players: players))
 
 
-print(teams.0)
-print(teams.1)
-print(teams.2)
+
+// Main Application
+let teams = makeTeam(from: sortPlayersByHeight(players: players))
+let letters = makeLetters(from: teams)
+let sharks = teams.0
+let dragons = teams.1
+let raptors = teams.2
+
+// Output the letters of each team
+outputLetters(letters: letters)
+
+// Output the average height of each team
+print("-----------------Average Heigth-------------------")
+print(reportAverageHeightFor(team: sharks, name: "Sharks"))
+print(reportAverageHeightFor(team: dragons, name: "Dragons"))
+print(reportAverageHeightFor(team: raptors, name: "Raptors"))
